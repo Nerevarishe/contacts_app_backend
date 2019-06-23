e83c0b1c34c85120c784962cc2f2e80c3791cd52
drugstore <drugstore@kassa05-01.apteka-lumen.ru>
2019-06-23 11:29:27 +0300

    adding error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,9 @@ def create_app(config_class=Config):
 
     db.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.contacts import bp as contacts_bp
     app.register_blueprint(contacts_bp)
 
